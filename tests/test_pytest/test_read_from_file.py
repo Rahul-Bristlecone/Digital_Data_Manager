c310d553e14ed6f72ab7efb6c50051811c8d2d70
Rahul Sharma <rsshrma92@hotmail.com>
2024-09-16 14:15:54 +0530

    framework

---
+++ tests/test_pytest/test_read_from_file.py
@@ -0,0 +1,10 @@
+import pytest
+from utils.util import read_data
+
+
+# This time data in parameter is being supplied from the data file
+
+# As the data is in column form, so a,b,c,d denote column name
+@pytest.mark.parametrize("a,b,c,d", read_data())
+def test_read_data(a,b,c,d):
+    print (b)
