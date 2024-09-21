d4ffbfa71964a4ce1e42f4de0d09f73f3ea6a229
Rahul Sharma <rsshrma92@hotmail.com>
2024-09-21 18:11:53 +0530

    connection

--- android_replica/response_handling.py
+++ android_replica/response_handling.py
@@ -1,34 +1,45 @@
 import configparser
 
 import requests
+import csv
 from pathlib import Path
+from io import StringIO
+import pandas as pd
+from select import error
 
 
 class ProductTable():
     file_name = "hostname.ini"
     directory_name = "config"
-    config =configparser.ConfigParser()
+    config = configparser.ConfigParser()
 
     def __init__(self):
         self.parent_path = Path(__file__).parent.parent
         self.original_file = self.parent_path.joinpath(self.directory_name).joinpath(self.file_name)
+        self.config.read(self.original_file)
 
-    def setup_connection(self):
-        hostname = self.config.read(self.original_file)["ABS_warehouse_hostname"]["hostname"]
-        port = self.config.read(self.original_file)["ABS_warehouse_hostname"]["port"]
+        hostname = self.config["ABS_warehouse_hostname"]["hostname"]
+        port = self.config["ABS_warehouse_hostname"]["port"]
         endpoint = "/PRODUCTS"
-        url = "http://" + hostname + ":" + port + endpoint
-
-        headers = {
-            "user_agent": "AbsScan/1.09"
+        self.url = "http://" + hostname + ":" + port + endpoint
+        self.headers = {
+            "User-Agent": "AbsScan/1.09"
         }
 
-        response = requests.get(url, headers=headers)
+    def setup_connection(self):
+        response = requests.get(self.url, headers=self.headers)
+        response_data = StringIO(response.text)
+        reader = pd.read_csv(response_data, sep='\t')
 
-        return response
+        if reader['6'].duplicated().any():
+            print("could not save due to duplicated data")
+
+        else:
+            reader.to_csv("/new.dat", sep="\t", index=False)
+
+        return reader
 
 
 if __name__ == "__main__":
     obj = ProductTable()
     print(obj.setup_connection())
-
