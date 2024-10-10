6bcfdec3aaa265401b21804953c541e6ab1e144b
Rahul Sharma <rsshrma92@hotmail.com>
2024-10-10 20:17:10 +0530

    download changes

--- android_replica/download_orders.py
+++ android_replica/download_orders.py
@@ -1,19 +1,12 @@
 # warehouse
 import requests
+from pandas import ExcelFile
+
 from get_orders import GetOrders
 from utils.product_table_utility import ProductTableInitializer
 
 
-# fetch orderId from response
-def order_id_from_response():
-    order_ids = set()
-    get_order_inst = GetOrders()
-    order_list = get_order_inst.fetch_outstanding_orders().strip().split('\n')
-    for list_of_orders in order_list:
-        order_detail = list_of_orders.strip().split('\t')
-        order_ids.add(order_detail[1])
 
-    return order_ids
 
 
 class DownloadOrders:
@@ -21,8 +14,20 @@ class DownloadOrders:
         self.file_path = "output/download_orders.dat"
         self.table_inst = ProductTableInitializer()
         self.url, self.headers = self.table_inst.get_url_and_headers("downloads")
+        self.order_ids = self.order_id_from_response()
+
 
     # fetch orderId from response
+    @staticmethod  # static method does not require an instance to call it
+    def order_id_from_response():
+        order_ids = set()
+        get_order_inst = GetOrders()
+        order_list = get_order_inst.fetch_outstanding_orders().strip().split('\n')
+        for list_of_orders in order_list:
+            order_detail = list_of_orders.strip().split('\t')
+            order_ids.add(order_detail[1])
+
+        return order_ids
 
     # fetch orderId from file
     def order_id_from_file(self):
@@ -31,16 +36,23 @@ class DownloadOrders:
 
     def downloaded_orders_details(self):
         open(self.file_path, 'w').close()
-        for order_id in order_id_from_response():
+        response = None
+
+        for order_id in self.order_ids:
             new_url = f"{self.url}/{order_id}"
-            response = requests.get(new_url, headers=self.headers)
+            try:
+                response = requests.get(new_url, headers=self.headers)
+                response.raise_for_status()
+            except requests.RequestException as e:
+                print("error fetching data for order id ",order_id , e)
+                continue
+
 
             with open(self.file_path, 'a') as download_file:
                 download_file.write(response.text)
 
-        return response.status_code
+        return response.status_code if response else None
 
 if __name__ == "__main__":
     download_order_inst = DownloadOrders()
-    print(order_id_from_response())
     print(download_order_inst.downloaded_orders_details())
